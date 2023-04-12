704755141321889ca676e8f06c44aca0b3c1b152
Adinda Kurnia Rifanti <adindakkr@gmail.com>
2023-04-12 13:15:23 +0700

    adinda_changes3

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -5,7 +5,7 @@
         <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Adinda Kurnia Rifanti</a>
+        <a href="#" class="d-block">Kelompok 8</a>
       </div>
     </div>
 
